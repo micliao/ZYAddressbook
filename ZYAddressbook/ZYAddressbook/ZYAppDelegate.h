fa8f11757e2b7edb5b6b2fafc49708f564c573c0
micliao <micliao@elemeNTdeMac.local>
2014-06-24 17:54:48 +0800

    新增项目文件

---
+++ ZYAddressbook/ZYAddressbook/ZYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZYAppDelegate.h
+//  ZYAddressbook
+//
+//  Created by micliao on 14-6-24.
+//  Copyright (c) 2014年 zy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZYAddressbook/ZYAddressbook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZYAddressbook
+//
+//  Created by micliao on 14-6-24.
+//  Copyright (c) 2014年 zy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZYAppDelegate class]));
+    }
+}
